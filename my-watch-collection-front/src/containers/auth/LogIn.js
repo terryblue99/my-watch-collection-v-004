def2da440f010f180491bc1472fe33e1c9aed30e
terryblue99 <terryblue@comcast.net>
2020-06-11 18:29:05 -0500

    reformated hooks

--- my-watch-collection-front/src/containers/auth/LogIn.js
+++ my-watch-collection-front/src/containers/auth/LogIn.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { useState, useSelector, useDispatch } from 'react-redux'
+import React, { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
 import logo from '../../images/logo.jpg'
 import { logInAction } from "../../actions/currentUserActions.js"
