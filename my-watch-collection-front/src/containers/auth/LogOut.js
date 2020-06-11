9e19ef145578af3ec76b647774809475d40d0452
terryblue99 <terryblue@comcast.net>
2020-06-11 17:12:21 -0500

    convert LogOut to functional component with hooks

--- my-watch-collection-front/src/containers/auth/LogOut.js
+++ my-watch-collection-front/src/containers/auth/LogOut.js
@@ -1,14 +1,11 @@
-import { Component } from 'react'
-import { connect } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { logOutAction } from "../../actions/currentUserActions.js"
 import RedirectTo from '../../components/RedirectTo'
 
-class LogOut extends Component {
-  
-  render() {
-    this.props.logOutAction()
-    return RedirectTo('/home')
-  } 
+const LogOut = () => {
+  const dispatch = useDispatch()
+  dispatch(logOutAction())
+  return RedirectTo('/home')
 }
 
-export default connect(null, { logOutAction })(LogOut)
+export default LogOut
