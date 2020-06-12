3e5d41ac021a358963ed04d9aad2176ef10fbd09
terryblue99 <terryblue@comcast.net>
2020-06-12 17:53:29 -0500

    amend v3 to v4

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   namespace :api, defaults: {format: :json} do
-    namespace :v3 do
+    namespace :v4 do
       resources :sessions, only: [:create]
       resources :registrations, only: [:create, :destroy]
       delete :logout, to: 'sessions#logout'
