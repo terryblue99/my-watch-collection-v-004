ae87b2416de651e8381ec4535a8644decc78083c
terryblue99 <terryblue@comcast.net>
2020-06-13 13:46:43 -0500

    add navbar

---
+++ my-watch-collection-front/src/containers/NavBar.js
@@ -0,0 +1,127 @@
+import React, { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { NavLink } from 'react-router-dom'
+import { searchWatchesAction } from '../actions/watchesActions'
+import ClearForm from '../components/ClearForm'
+import RedirectToWithState from '../components/RedirectToWithState'
+
+const NavBar = () => {
+
+  const [searchData, setSearchData] = useState({isSearchRequested: false, searchText: ''})
+  const user = useSelector(state => state.currentUser)
+  const watches = useSelector(state => state.myWatches.watches)
+  const dispatch = useDispatch()
+
+  handleChange = (event) => {
+        setSearchData(prevSearchData => {
+            return {
+              ...prevSearchData,
+              searchText: event.target.value
+            }
+        })
+  }
+
+  handleSearch = (event) => {
+    event.preventDefault()
+    dispatch(searchWatchesAction(searchData.searchText))
+    setSearchData(prevSearchData => {
+      return {
+        ...prevSearchData,
+        isSearchRequested: true
+      }
+    })
+  }
+    
+  if (searchData.isSearchRequested &&
+      watches.length > 0) {
+        setSearchData(prevSearchData => {
+          return {
+            ...prevSearchData,
+            isSearchRequested: false,
+            searchText: ''
+          }
+        }) 
+      // Clear the form
+      ClearForm('Nav-search-form')
+      // Display list from the search on the dashboard
+      return  RedirectToWithState(
+                                    '/dashboard',
+                                    {
+                                      isFromNavBar: true,    
+                                      isSearchSuccessful: true
+                                    } 
+                                  )
+      
+  } else if (searchData.isSearchRequested &&
+             watches.length === 0) 
+              {
+                setSearchData(prevSearchData => {
+                  return {
+                    ...prevSearchData,
+                    isSearchRequested: false,
+                    searchText: ''
+                  }
+                })
+                // Clear the form
+                ClearForm('Nav-search-form')
+
+                alert('Search not found. Please correct and try again!')
+                // Display original list on the dashboard
+                return  RedirectToWithState (
+                                              '/dashboard',
+                                              {
+                                                isFromNavBar: true,    
+                                                isSearchFailed: true
+                                              } 
+                                            )
+              }
+
+  return (
+    
+    <div className='Nav-header'>
+
+      <div className='Nav-container'>   
+
+        <div className='Nav-logo Gold-color' to='/dashboard'>
+          My Watch Collection
+        </div>
+      
+        <nav className='Nav'>
+
+          <div className='Nav-links'> 
+            {user.logged_in} {
+              <>
+                <div  className='Nav-logged_in-as'>
+                  Logged in as: {user.email}
+                </div>
+                <NavLink  className='Nav-log_out-link Cornsilk-color' to='/logout'> 
+                  Log Out   
+                </NavLink>
+                <NavLink  className='Nav-link Nav-edit-profile-link Cornsilk-color' to='/edit_profile'
+                          onClick={() => {className=' active'}}>
+                  Edit Profile   
+                </NavLink>
+              </>
+            }
+          </div>
+        </nav>
+
+        <form id='Nav-search-form'
+          onSubmit={handleSearch}
+        >
+          <input className='Nav-search-input Center-text'
+            type='text'
+            name='watch_search'
+            placeholder='Search My Watch Collection'
+            onChange={handleChange}
+          /> 
+          <button className='Search-button Button-text' type='submit'><b>Search</b></button> 
+        </form>
+      </div>
+      
+    </div>   
+  
+    )
+}
+
+export default NavBar
